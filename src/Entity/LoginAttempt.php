ce4d4550cf55956407e920058bf003e4710345e2
fpodev <fpo@gmx.fr>
2020-10-20 17:25:56 +0200

    creation

---
+++ src/Entity/LoginAttempt.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LoginAttemptRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=LoginAttemptRepository::class)
+ */
+class LoginAttempt
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50, nullable=true)
+     */
+    private $ipAdress;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $username;
+
+    public function __construct(?string $username)
+    {        
+        $this->username = $username;
+        $this->date = new \DateTimeImmutable('now');
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }   
+    public function getDate(): ?\DateTimeImmutable
+    {
+        return $this->date;
+    }   
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+}
