7b4c66d38a406d47fc51958b1b7213bf4bb7bd06
fpodev <fpo@gmx.fr>
2020-10-20 17:29:55 +0200

    modif suite changement entité

--- src/Form/UsersType.php
+++ src/Form/UsersType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Users;
+use App\Entity\Ville;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -37,12 +39,9 @@ class UsersType extends AbstractType
                     'class' => 'form-control'  
                 ]           
             ])
-            ->add('site', TextType::class, [
-                'required' => true,  
-                'attr' => [
-                    'class' => 'form-control'  
-                ]           
-            ]) 
+            ->add('ville', EntityType::class, [
+                'class' => Ville::class,
+                ])        
             ->add('roles', ChoiceType::class, [
                 'choices' => [
                     'Administrateur' => 'ROLE_ADMIN',
