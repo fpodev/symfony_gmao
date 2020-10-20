32463b2fda1e5a7bf9f838cd48cd98b77a72bb91
fpodev <fpo@gmx.fr>
2020-10-20 17:24:54 +0200

    Multiple changement dont majeur dans l'index

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -4,15 +4,19 @@ namespace App\Controller;
 
 use App\Entity\Users;
 use App\Form\UsersType;
+use App\Form\ChangePasswordType;
+use App\Repository\LoginAttemptRepository;
 use App\Repository\UsersRepository;
+use DateTime;
 use Symfony\Component\Mime\Address;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\VarDumper\VarDumper;
 
 /**
  * @Route("/users", name="users_")
@@ -22,24 +26,51 @@ class UsersController extends AbstractController
     /**
      * @Route("/", name="home", methods={"GET"})
      */
-    public function index(UsersRepository $usersRepository): Response
-    {
-        return $this->render('users/index.html.twig', [
-            'users' => $usersRepository->findAll(),
-        ]);
+    public function index(Request $request, UsersRepository $usersRepository, LoginAttemptRepository $loginAttemptReposity): Response
+    {   
+        $session = $request->getSession();
+
+        $user = $usersRepository->findOneBy(['id' => $session->get('id')]); 
+        $entityManager = $this->getDoctrine()->getManager();
+
+            $loginAttempt = $loginAttemptReposity->findBy(['username' => $user->getEmail()]);
+            foreach ($loginAttempt as $key) {
+                $entityManager->remove($key);            
+               };               
+            
+            $entityManager->flush();
+
+        $today = new DateTime('now');        
+        $actived = $user->getActivateDate(); 
+        $valid = $actived ? $actived : $today;
+        $duration = $today->diff($valid);        
+        $expired = $duration->format('%a');        
+      
+        if($actived == null || $expired >= '30'){           
+            $form = $this->createForm(ChangePasswordType::class);                                       
+            
+             return $this->render('change_password/index.html.twig',[
+                 'user' => $user,
+                 'valid' => $valid,
+                 'form' => $form->createView()
+             ]);
+        }else{
+            return $this->render('users/home.html.twig', [
+                'expired' => $expired,
+            ]);
+        }            
     }
 
     /**
      *
      * @Route("/new", name="new", methods={"GET","POST"})
      */
-    public function new(Request $request, MailerInterface $mailer, UserPasswordEncoderInterface $passEncode): Response
-    {
-        $user = new Users();
-        
+    public function new(Request $request, MailerInterface $mailer, UserPasswordEncoderInterface $passEncode, Users $user ): Response
+    {    
+             
         $form = $this->createForm(UsersType::class, $user);
         $form->handleRequest($request);
-
+           
         if ($form->isSubmitted() && $form->isValid()) {
             #Génére un mot de passe aléatoire
             $pass = substr(str_shuffle(
@@ -70,16 +101,16 @@ class UsersController extends AbstractController
                 $mailer->send($email);
 
             return $this->redirectToRoute('users_index');
-        }
-
-        return $this->render('users/new.html.twig', [
-            'user' => $user,
-            'form' => $form->createView(),
-        ]);
+        }else{
+            return $this->render('users/new.html.twig', [
+                'user' => $user,
+                'form' => $form->createView(),
+            ]);
+        }        
     }
 
     /**
-     * @Route("/{id}", name="users_show", methods={"GET"})
+     * @Route("/{id}", name="_show", methods={"GET"})
      */
     public function show(Users $user): Response
     {
@@ -89,7 +120,7 @@ class UsersController extends AbstractController
     }
 
     /**
-     * @Route("/{id}/edit", name="users_edit", methods={"GET","POST"})
+     * @Route("/{id}/edit", name="users_edit", methods={"POST"})
      */
     public function edit(Request $request, Users $user): Response
     {
@@ -99,17 +130,17 @@ class UsersController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('users_index');
-        }
-
-        return $this->render('users/edit.html.twig', [
-            'user' => $user,
-            'form' => $form->createView(),
-        ]);
+            return $this->redirectToRoute('users_home');
+        }else{
+            return $this->render('users/edit.html.twig', [
+                'user' => $user,
+                'form' => $form->createView(),
+            ]);
+        }        
     }
 
     /**
-     * @Route("/{id}", name="users_delete", methods={"DELETE"})
+     * @Route("/{id}", name="delete", methods={"DELETE"})
      */
     public function delete(Request $request, Users $user): Response
     {
@@ -122,24 +153,35 @@ class UsersController extends AbstractController
         return $this->redirectToRoute('users_index');
     }
      /**
-     * @Route("/{id}/users/change-password", name="change_password", methods={"GET","POST"})
+     * @Route("/change/{id<[0-9]+>}", name="change")
      */
-    public function changePassword(Request $request, UserPasswordEncoderInterface $passEncode, Users $user): Response
-    {
-        if($request)
-        $form = $this->createForm(UsersType::class, $user);
-        $form->handleRequest($request);
+    public function changePassword(Request $request, UserPasswordEncoderInterface $passEncode): Response  
+    {            
+        if($request->isMethod('POST')) { 
+            $post = $request->request;            
+           
+           foreach ($post as $key) {               
+            $value = $key['new_password'] ;
+            $pass = $key['password'];             
+           };    
+        }   
+           $user = $this->getUser();          
+              
+            if($passEncode->isPasswordValid($user, $pass) && $value['first'] === $value['second']) {                
+                    $user->setPassword($passEncode->encodePassword($user, $value['first']));
+                    $user->setActivateDate(new \DateTime('now'));
+                    $em = $this->getDoctrine()->getManager();
+                    $em->flush();
 
-        if ($form->isSubmitted() && $form->isValid()) {
+                    $this->addFlash('valid', 'Vous avez bien modifié votre mot de passe');                    
+                    return $this->redirectToRoute('users_home');
+                }else{ 
 
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('users_index');
-        }
-
-        return $this->render('users/edit.html.twig', [
-            'user' => $user,
-            'form' => $form->createView(),
-        ]);
+                    $this->addFlash('erreur', 'Votre ancien mot de passe ou la réptition du nouveau du mot de passe sont faux. Veuillez réessayer');                    
+            
+                    return $this->redirectToRoute('users_home');    
+               
+                }
     }
+    
 }
