a0109a5b74c8ebd69848dfec0e9c2fba9da9fcab
fpodev <fpo@gmx.fr>
2020-10-20 17:28:07 +0200

    Creation

---
+++ src/Entity/Ville.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VilleRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=VilleRepository::class)
+ */
+class Ville
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adress;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $zip_code;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Users::class, inversedBy="villes")
+     * @ORM\JoinColumn(nullable=true)
+     */
+    private $contact; 
+
+    public function __toString()
+    {
+        return $this->name;
+    }
+    
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAdress(): ?string
+    {
+        return $this->adress;
+    }
+
+    public function setAdress(string $adress): self
+    {
+        $this->adress = $adress;
+
+        return $this;
+    }
+
+    public function getZipCode(): ?string
+    {
+        return $this->zip_code;
+    }
+
+    public function setZipCode(string $zip_code): self
+    {
+        $this->zip_code = $zip_code;
+
+        return $this;
+    }
+
+    public function getContact(): ?Users
+    {
+        return $this->contact;
+    }
+
+    public function setContact(?Users $contact): self
+    {
+        $this->contact = $contact;
+
+        return $this;
+    }
+    
+    
+}
