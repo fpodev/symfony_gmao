75eaf0dda8a08ae7e39dce2eb371f1514dccbfd0
fpodev <fpo@gmx.fr>
2020-11-14 16:47:44 +0100

    Divers modification

--- src/Controller/VilleController.php
+++ src/Controller/VilleController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 use App\Entity\Users;
 use App\Entity\Ville;
 use App\Form\VilleType;
+use App\Entity\Building;
+use App\Form\BuildingType;
 use App\Repository\VilleRepository;
-use ProxyManager\Example\GhostObjectSkippedProperties\User;
+use App\Repository\BuildingRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,6 +19,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
  */
 class VilleController extends AbstractController
 {
+    public const DEFAULT_BUILDING = 'Bâtiment A';
+
     /**
      * @Route("/", name="ville_index", methods={"GET"})
      */
@@ -30,25 +34,26 @@ class VilleController extends AbstractController
     /**
      * @Route("/new", name="ville_new", methods={"GET","POST"})
      */
-    public function new(Request $request, VilleRepository $derVille): Response
+    public function new(Request $request, VilleRepository $newVille): Response
     {
-        $userVille = new Users();
         $ville = new Ville();
+        $building = new Building();
+        $user = new Users();
         $form = $this->createForm(VilleType::class, $ville);
-        $form->handleRequest($request);        
-        
-        if ($form->isSubmitted() && $form->isValid()) {
-            
-            $entityManager = $this->getDoctrine()->getManager();            
-            $entityManager->persist($ville);
-            $entityManager->flush(); 
+        $form->handleRequest($request);
 
-            $idVille = $derVille->findOneby([], ['id' => 'desc']);
-            $id = $idVille->getId();
-            $user = $idVille->getContact();
-            $user->setVille($id);         
+        if ($form->isSubmitted() && $form->isValid()) {       
 
-            $entityManager = $this->getDoctrine()->getManager();        
+            $building->setName(self::DEFAULT_BUILDING); 
+            $building->setVille($ville);
+            $ville->getBuilding()->add($building);            
+         
+            $user = $ville->getContact();       
+            $user->setVille($ville);
+            $ville->getUsers()->add($user);        
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($ville);       
             $entityManager->flush();
 
             return $this->redirectToRoute('ville_index');
@@ -63,10 +68,11 @@ class VilleController extends AbstractController
     /**
      * @Route("/{id}", name="ville_show", methods={"GET"})
      */
-    public function show(Ville $ville): Response
+    public function show(Ville $ville, BuildingRepository $Building): Response
     {
         return $this->render('ville/show.html.twig', [
             'ville' => $ville,
+            'building' => $Building->findBy(['ville' => $ville->getId()]),
         ]);
     }
 
@@ -74,16 +80,44 @@ class VilleController extends AbstractController
      * @Route("/{id}/edit", name="ville_edit", methods={"GET","POST"})
      */
     public function edit(Request $request, Ville $ville): Response
-    {
+    {            
         $form = $this->createForm(VilleType::class, $ville);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $user = $ville->getContact();            
+            $user->setVille($ville);            
             $this->getDoctrine()->getManager()->flush();
 
             return $this->redirectToRoute('ville_index');
-        }        
+        }
+
         return $this->render('ville/edit.html.twig', [
+            'ville' => $ville,
+            'form' => $form->createView(),
+        ]);
+    }
+    /**
+     * @Route("/{id}/addBuilding", name="ville_addBuilding", methods={"GET","POST"})
+     */
+    public function addBuilding(Request $request, Ville $ville, $id): Response
+    {    
+        $building = new Building();        
+        $form = $this->createForm(BuildingType::class);
+        $form->handleRequest($request);
+       
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $building->setName($form->get('name')->getData());                     
+            $building->setVille($ville);                                 
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($building);       
+            $entityManager->flush();
+
+            return $this->redirectToRoute('ville_index');
+        }
+
+        return $this->render('ville/addBuilding.html.twig', [
             'ville' => $ville,
             'form' => $form->createView(),
         ]);
@@ -95,11 +129,13 @@ class VilleController extends AbstractController
     public function delete(Request $request, Ville $ville): Response
     {
         if ($this->isCsrfTokenValid('delete'.$ville->getId(), $request->request->get('_token'))) {
+            $user = $ville->getContact();
+            $user->setVille(null);            
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($ville);
             $entityManager->flush();
         }
 
         return $this->redirectToRoute('ville_index');
-    }
+    }    
 }
